98914f3e5c9965b11d8fb5cc59fcd5c0b91cfe0c
Jose Ruben <jose.ruben.machado.vega@gmail.com>
2022-08-24 18:37:34 +0200

    adding withimage library

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withImages = require("next-images");
+const nextConfig = withImages({
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    disableStaticImages: true,
     domains: [
+      'localhost:3000',
       'staging.3w.ucorreos.com',
-      '3w.ucorreos.com',
-      'staging.3w.ucorreos.com/*'
+      '3w.ucorreos.com'
     ],
-  }
+  },
+  webpack(config, options) {
+    return config;
 }
+  
+})
 
 module.exports = nextConfig
