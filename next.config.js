01a8e772ee7d7a60838a48b339034f5d96e50971
Jose Ruben <jose.ruben.machado.vega@gmail.com>
2022-08-24 17:52:43 +0200

    adding image white list

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'staging.3w.ucorreos.com',
+      '3w.ucorreos.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
